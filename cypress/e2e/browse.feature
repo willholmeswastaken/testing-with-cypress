5f9d87b1f0a5b27fb7ed5f805521dc6105ce7c08
Will Holmes <willholmes97@icloud.com>
2023-02-17 13:25:46 +0000

    Added explicit/implicit examples

--- cypress/e2e/browse.feature
+++ cypress/e2e/browse.feature
@@ -1,7 +1,12 @@
 Feature: Browsing
-    Scenario: I want to be able to click the button on the landing page
+    Scenario: I want to be able to click the button on the landing page with explicit mocking
         Given I am on the landing page
         And the api request url "/api/hello" and the verb "GET" returns the fixture "hello-success.json" with a status code of 200
         When I click the button
         Then I should get an alert with the name "Will"
         And the api request url "/api/hello" and the verb "GET" should have been made
+
+    Scenario: I want to be able to click the button on the landing page with implicit mocking
+        Given I am on the landing page
+        When I click the button to say hello
+        Then I should get an alert with the name "Will" with validation
--- cypress/e2e/browse.ts
+++ cypress/e2e/browse.ts
@@ -23,6 +23,16 @@ When("I click the button", () => {
   cy.get("button").click();
 });
 
+When("I click the button to say hello", () => {
+  cy.intercept("GET", "/api/hello", (req) => {
+    req.reply((res) => {
+      res.statusCode = 200;
+      res.send({ fixture: "hello-success.json" });
+    });
+  }).as("GET/api/hello");
+  cy.get("button").click();
+});
+
 Then(
   "the api request url {string} and the verb {string} should have been made",
   (apiUrl: string, verb: Method) => {
@@ -34,3 +44,12 @@ Then("I should get an alert with the name {string}", (name: string) => {
   cy.get("#alert").should("be.visible");
   cy.get("#alert").invoke("text").should("eq", name);
 });
+
+Then(
+  "I should get an alert with the name {string} with validation",
+  (name: string) => {
+    cy.wait("@GET/api/hello");
+    cy.get("#alert").should("be.visible");
+    cy.get("#alert").invoke("text").should("eq", name);
+  }
+);
